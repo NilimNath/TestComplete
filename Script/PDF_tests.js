5bb8931556f1b1caf40d64e2a23254ebbf29c38d
Nilim Nath <nath.nilim@gmail.com>
2025-10-07 10:49:16 +1100

    Updated the files item

---
+++ Script/PDF_tests.js
@@ -0,0 +1,20 @@
+﻿function getText()
+{
+  var text = PDF.ConvertToText("C:\Users\nilim.nath\Downloads\pdfprint_document.pdf");
+  
+//  Log.Message(text);
+  if (text.BlockCount > 0)
+        {
+          Log.AppendFolder("Recognized text by blocks");
+          for (var i = 0; i < text.BlockCount; i++)
+          {
+            Log.Message(text.Block(i).Text);
+          }
+          Log.PopLogFolder();
+        }
+  
+  
+  var image = OCR.Recognize(Sys.Browser("chrome").Page("file:///C:/Users/nilim.nath/Downloads/pdfprint_document.pdf"));
+  
+  Log.Message(image.AsTable());
+}
